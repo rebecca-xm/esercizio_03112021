35a6efd000844986208ec3cd829f1bfa63aa3092
Rebecca Mollica <rebecca.mollica@live.it>
2021-11-03 16:33:02 +0100

    esercizio senza pop button

---
+++ Esercizio/index.js
@@ -0,0 +1,28 @@
+const q = (selector) => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = q("#boxForm");
+    render(form, quote);
+});
+
+const render = (container, items) => {
+    const elements = items.map(
+        (element) => {
+            if (element.completed === true) {
+                return `<li>
+                <label for="search">  ${element.title} <input type="checkbox" checked /></label>
+                </li>`
+
+
+            } else {
+                return `<li>
+                <label for="search">  ${element.title} <input type="checkbox"/></label>
+                </li>`
+
+            }
+        }
+    );
+    const output = elements.join(" ");
+
+    container.innerHTML = output;
+};
